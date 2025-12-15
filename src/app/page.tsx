4c893aadbe89f5c75c546a1dff59c9076a3d487c
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-15 13:00:54 +0000

    add the left bar now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 import Header from '@/components/layout/header';
 import MapView from '@/components/sections/map-view';
+import PropertyList from '@/components/sections/property-list';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarProvider,
+} from '@/components/ui/sidebar';
 
 export default function Home() {
   return (
+    <SidebarProvider>
       <div className="flex flex-col h-screen bg-background">
         <Header />
-        <main className="flex-1 relative">
-          <MapView />
-        </main>
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar side="left" className="w-96 hidden md:flex flex-col border-r">
+            <SidebarContent>
+              <PropertyList />
+            </SidebarContent>
+          </Sidebar>
+          <main className="flex-1 relative">
+            <MapView />
+          </main>
+        </div>
       </div>
+    </SidebarProvider>
   );
 }
