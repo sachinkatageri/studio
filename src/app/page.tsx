607f1cd6f4654e00b1552bafec1c75f6db40187e
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-15 12:52:15 +0000

    the brakponits are missing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from '@/components/layout/header';
 import MapView from '@/components/sections/map-view';
 import PropertyList from '@/components/sections/property-list';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { Sidebar, SidebarContent, SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Home() {
   return (
@@ -9,9 +9,11 @@ export default function Home() {
       <div className="flex flex-col h-screen bg-background">
         <Header />
         <div className="flex flex-1 overflow-hidden">
-          <div className="w-96 hidden md:flex flex-col border-r">
-            <PropertyList />
-          </div>
+          <Sidebar side="left" className="w-96 hidden md:flex flex-col border-r">
+             <SidebarContent>
+                <PropertyList />
+             </SidebarContent>
+          </Sidebar>
           <main className="flex-1 relative">
             <MapView />
           </main>
