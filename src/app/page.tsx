b1be9fdd99623b874ac9df7b7a0bec8184e2fc08
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-15 13:00:23 +0000

    expand this to full view (_for element <SidebarProvider>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Header from '@/components/layout/header';
 import MapView from '@/components/sections/map-view';
-import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Home() {
   return (
-    <SidebarProvider>
       <div className="flex flex-col h-screen bg-background">
         <Header />
         <main className="flex-1 relative">
           <MapView />
         </main>
       </div>
-    </SidebarProvider>
   );
 }
