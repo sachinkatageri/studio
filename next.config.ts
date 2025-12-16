8076bcc3067441f2896b0a028bc608e987a646f4
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-16 17:11:20 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://cdnjs.c

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdnjs.cloudflare.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
