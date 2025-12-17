7442106276b40e40b3cf7ca5159b11c743ca35c1
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-17 11:04:06 +0000

    remove the phone and option icon (_for element <MobileHeader>_)

--- src/app/property/[id]/_components/property-page-content.tsx
+++ src/app/property/[id]/_components/property-page-content.tsx
@@ -42,13 +42,7 @@ const MobileHeader = ({ property }: { property: typeof properties[0] }) => {
                     <Search className="h-5 w-5" />
                 </Button>
                 <Button variant="ghost" size="icon">
-                    <Phone className="h-5 w-5" />
-                </Button>
-                <Button variant="ghost" size="icon">
                     <Share2 className="h-5 w-5" />
-                </Button>
-                <Button variant="ghost" size="icon">
-                    <MoreVertical className="h-5 w-5" />
                 </Button>
             </div>
         </div>
