72d641b7c667eb8df82abe7275d6924b5e9ba553
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-22 18:08:21 +0000

    add scoll to all these cards in the list view (_for element <CardContent

--- src/components/sections/property-list.tsx
+++ src/components/sections/property-list.tsx
@@ -146,30 +146,28 @@ export default function PropertyList({ onSelectProperty, selectedPropertyId, set
                     </div>
                 </div>
 
-                <div className="flex-1 relative">
-                    <ScrollArea className="absolute inset-0">
-                        <TabsContent value="all" className="mt-0">
-                            <div>
-                                {properties.map((property) => (
-                                    <PropertyCard key={property.id} property={property} onSelect={onSelectProperty} isSelected={selectedPropertyId === property.id} />
-                                ))}
-                            </div>
-                        </TabsContent>
-                        <TabsContent value="commercial" className="mt-0">
-                            <div>
-                                {commercialProperties.map((property) => (
-                                    <PropertyCard key={property.id} property={property} onSelect={onSelectProperty} isSelected={selectedPropertyId === property.id} />
-                                ))}
-                            </div>
-                        </TabsContent>
-                        <TabsContent value="residential" className="mt-0">
-                            <div>
-                                {residentialProperties.map((property) => (
-                                    <PropertyCard key={property.id} property={property} onSelect={onSelectProperty} isSelected={selectedPropertyId === property.id} />
-                                ))}
-                            </div>
-                        </TabsContent>
-                    </ScrollArea>
+                <div className="flex-1 overflow-y-auto">
+                    <TabsContent value="all" className="mt-0">
+                        <div>
+                            {properties.map((property) => (
+                                <PropertyCard key={property.id} property={property} onSelect={onSelectProperty} isSelected={selectedPropertyId === property.id} />
+                            ))}
+                        </div>
+                    </TabsContent>
+                    <TabsContent value="commercial" className="mt-0">
+                        <div>
+                            {commercialProperties.map((property) => (
+                                <PropertyCard key={property.id} property={property} onSelect={onSelectProperty} isSelected={selectedPropertyId === property.id} />
+                            ))}
+                        </div>
+                    </TabsContent>
+                    <TabsContent value="residential" className="mt-0">
+                        <div>
+                            {residentialProperties.map((property) => (
+                                <PropertyCard key={property.id} property={property} onSelect={onSelectProperty} isSelected={selectedPropertyId === property.id} />
+                            ))}
+                        </div>
+                    </TabsContent>
                 </div>
             </Tabs>
         </div>
