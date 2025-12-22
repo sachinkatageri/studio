37ee3c7d2d69073646f3979c1f867891ca41f870
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-22 09:00:49 +0000

    change the z index of the list view sheet only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,8 +96,8 @@ export default function Home() {
           <Footer />
         </div>
         <Drawer.Portal>
-          <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="bg-background flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
+          <Drawer.Overlay className="fixed inset-0 bg-black/40 z-50" />
+          <Drawer.Content className="bg-background flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0 z-50">
             <div className="p-4 bg-background rounded-t-[10px] flex-1 flex flex-col">
               <div className="flex items-center justify-between mb-4">
                 <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-muted-foreground/20" />
