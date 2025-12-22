658a32d862b679c7f6e4b97fae41ae09b751a7bf
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-22 20:22:01 +0000

    in the mobile view show the card pop up 10% from the below

--- src/components/sections/property-details-sheet.tsx
+++ src/components/sections/property-details-sheet.tsx
@@ -36,7 +36,7 @@ export function PropertyDetailsSheet({ propertyId, onClose, onViewDetails }: Pro
 
   return (
     <Sheet open={open} onOpenChange={(open) => !open && onClose()}>
-      <SheetContent side="bottom" className="h-[85vh] max-h-[85vh] flex flex-col p-0 bg-transparent border-0">
+      <SheetContent side="bottom" className="h-auto flex flex-col p-0 bg-transparent border-0 bottom-[10%]">
         <SheetHeader className="sr-only">
           <SheetTitle>Property Details</SheetTitle>
           <SheetDescription>Details for the selected property.</SheetDescription>
