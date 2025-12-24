0e29ed8c235197f6db139fda94cc6dff680f802a
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-24 19:10:47 +0000

    here also breadcrumbs are not needed in the mobile view (_for element h1

--- src/app/builders/[id]/_components/builder-details-page.tsx
+++ src/app/builders/[id]/_components/builder-details-page.tsx
@@ -12,8 +12,10 @@ export default function BuilderDetailsPage({ builder }: { builder: Builder }) {
     return (
         <>
             <Header />
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-10 pt-14">
-                <Breadcrumbs builderName={builder.name} />
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-10 pt-20 md:pt-14">
+                <div className="hidden md:block">
+                    <Breadcrumbs builderName={builder.name} />
+                </div>
                 <h1 className="text-3xl font-bold mt-4">{builder.name}</h1>
                 <p className="text-muted-foreground mt-1">Explore projects, and more from {builder.name}.</p>
 
