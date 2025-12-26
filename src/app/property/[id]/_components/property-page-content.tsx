e8e13c1b46e5e33e33941d2e54d1f82f08945bed
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-26 11:58:35 +0000

    i want this view to be full dont waste space on the right side (_for ele

--- src/app/property/[id]/_components/property-page-content.tsx
+++ src/app/property/[id]/_components/property-page-content.tsx
@@ -104,15 +104,12 @@ export default function PropertyPageContent({ property }: PropertyPageContentPro
             <PropertyStickyNav />
 
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-4 pb-16 md:py-8">
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-12">
-                    <div className="lg:col-span-2 space-y-8">
+                <div className="lg:grid lg:grid-cols-3 lg:gap-12">
+                    <div className="lg:col-span-3 space-y-8">
                         <PropertyDetailsPanel property={property} />
                         <div className="lg:hidden">
                             <PropertyContactForm />
                         </div>
-                    </div>
-                    <div className="hidden lg:block">
-                       {/* This space is for the sticky contact form on desktop */}
                     </div>
                 </div>
 
