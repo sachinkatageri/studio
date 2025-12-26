1ff5ebce3f408254172e7b77d43f031e90773020
Sachin Katageri <sachin.autocrud@gmail.com>
2025-12-26 08:18:32 +0000

    Trukate or shorten these (_for element span_)

--- src/lib/amenities.ts
+++ src/lib/amenities.ts
@@ -12,48 +12,48 @@ export type Amenity = {
 
 export const allAmenities: Amenity[] = [
     // GUEST_SERVICES
-    { name: 'Guest check-in / registration', category: 'GUEST_SERVICES', icon: User },
-    { name: 'Delivery acceptance/notification', category: 'GUEST_SERVICES', icon: BellRing },
-    { name: 'Package notification', category: 'GUEST_SERVICES', icon: Package },
+    { name: 'Guest Check-in', category: 'GUEST_SERVICES', icon: User },
+    { name: 'Delivery Acceptance', category: 'GUEST_SERVICES', icon: BellRing },
+    { name: 'Package Notification', category: 'GUEST_SERVICES', icon: Package },
 
     // SECURITY
-    { name: 'Fire and safety protocols', category: 'SECURITY', icon: Siren },
-    { name: 'Guest management system', category: 'SECURITY', icon: User },
-    { name: 'Security guards and video surveillance', category: 'SECURITY', icon: Video },
-    { name: 'Keycard access', category: 'SECURITY', icon: KeyRound },
+    { name: 'Fire Safety', category: 'SECURITY', icon: Siren },
+    { name: 'Guest Management', category: 'SECURITY', icon: User },
+    { name: 'Video Surveillance', category: 'SECURITY', icon: Video },
+    { name: 'Keycard Access', category: 'SECURITY', icon: KeyRound },
 
     // FOOD_BEVERAGES
     { name: 'Tea', category: 'FOOD_BEVERAGES', icon: CupSoda },
     { name: 'Coffee', category: 'FOOD_BEVERAGES', icon: Coffee },
     { name: 'Water', category: 'FOOD_BEVERAGES', icon: GlassWater },
-    { name: 'Milk / Sweeteners', category: 'FOOD_BEVERAGES', icon: Milk },
-    { name: 'Cups, mugs and more', category: 'FOOD_BEVERAGES', icon: CupSoda },
-    { name: 'Pantry available 24/7', category: 'FOOD_BEVERAGES', icon: Clock },
+    { name: 'Milk & Sweeteners', category: 'FOOD_BEVERAGES', icon: Milk },
+    { name: 'Cups & Mugs', category: 'FOOD_BEVERAGES', icon: CupSoda },
+    { name: '24/7 Pantry', category: 'FOOD_BEVERAGES', icon: Clock },
     { name: 'Cafeteria', category: 'FOOD_BEVERAGES', icon: Building },
     { name: 'Food Vendor', category: 'FOOD_BEVERAGES', icon: User },
 
     // CLEANING
-    { name: 'Daily upkeep of bathrooms', category: 'CLEANING', icon: Trash2 },
-    { name: 'Nightly trash removal', category: 'CLEANING', icon: Trash2 },
-    { name: 'Deep - cleaning', category: 'CLEANING', icon: Trash2 },
-    { name: '24/7 general cleaning', category: 'CLEANING', icon: Clock },
-    { name: 'Pest extermination', category: 'CLEANING', icon: ShieldCheck },
+    { name: 'Daily Cleaning', category: 'CLEANING', icon: Trash2 },
+    { name: 'Trash Removal', category: 'CLEANING', icon: Trash2 },
+    { name: 'Deep Cleaning', category: 'CLEANING', icon: Trash2 },
+    { name: '24/7 Cleaning', category: 'CLEANING', icon: Clock },
+    { name: 'Pest Control', category: 'CLEANING', icon: ShieldCheck },
 
     // PRODUCTIVITY
     { name: 'High-Speed WiFi', category: 'PRODUCTIVITY', icon: Wifi },
-    { name: 'Tape and paper clips', category: 'PRODUCTIVITY', icon: Paperclip },
-    { name: 'Sticky notes', category: 'PRODUCTIVITY', icon: StickyNote },
-    { name: 'Printers and copies', category: 'PRODUCTIVITY', icon: Printer },
-    { name: 'Paper, pens, stapler', category: 'PRODUCTIVITY', icon: Pen },
-    { name: 'Paper shredding', category: 'PRODUCTIVITY', icon: Trash2 },
+    { name: 'Tape & Clips', category: 'PRODUCTIVITY', icon: Paperclip },
+    { name: 'Sticky Notes', category: 'PRODUCTIVITY', icon: StickyNote },
+    { name: 'Printing & Copying', category: 'PRODUCTIVITY', icon: Printer },
+    { name: 'Stationery', category: 'PRODUCTIVITY', icon: Pen },
+    { name: 'Paper Shredding', category: 'PRODUCTIVITY', icon: Trash2 },
     { name: 'Envelopes', category: 'PRODUCTIVITY', icon: Pen },
 
     // ON_CREDIT
-    { name: 'Phone booth access', category: 'ON_CREDIT', icon: Phone },
-    { name: 'Conference room access', category: 'ON_CREDIT', icon: Briefcase },
-    { name: 'Meeting/conference room', category: 'ON_CREDIT', icon: Briefcase },
-    { name: 'Open desk', category: 'ON_CREDIT', icon: User },
-    { name: 'Event sponsor', category: 'ON_CREDIT', icon: Handshake },
+    { name: 'Phone Booths', category: 'ON_CREDIT', icon: Phone },
+    { name: 'Conference Rooms', category: 'ON_CREDIT', icon: Briefcase },
+    { name: 'Meeting Rooms', category: 'ON_CREDIT', icon: Briefcase },
+    { name: 'Open Desk', category: 'ON_CREDIT', icon: User },
+    { name: 'Event Sponsor', category: 'ON_CREDIT', icon: Handshake },
 ];
 
 export const amenityIcons: { [key: string]: React.ElementType } = {
