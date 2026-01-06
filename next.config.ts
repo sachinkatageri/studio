0d47845933f26ff6b26d4446c892f6ed33da0c00
Sachin Katageri <sachin.autocrud@gmail.com>
2026-01-06 09:38:37 +0000

    in the production deployment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   eslint: {
     ignoreDuringBuilds: true,
@@ -78,8 +78,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
