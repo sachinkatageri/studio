829ae82a82be863e098590d35ed915ec1abe718d
Sachin Katageri <sachin.kumar.katageri@gmail.com>
2026-01-17 18:19:59 +0000

    make this full width now (_for element div_)

--- src/app/property/[id]/_components/property-page-content.tsx
+++ src/app/property/[id]/_components/property-page-content.tsx
@@ -115,15 +115,8 @@ export default function PropertyPageContent({ property }: PropertyPageContentPro
                 <PropertyStickyNav />
 
                 <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                    <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 md:gap-12">
-                        <div className="lg:col-span-8">
-                            <PropertyDetailsPanel property={property} />
-                        </div>
-                        <div className="lg:col-span-4">
-                            <div className="sticky top-48">
-                                 {/* On desktop, the contact form is now a popover */}
-                            </div>
-                        </div>
+                    <div>
+                        <PropertyDetailsPanel property={property} />
                     </div>
 
                     <div className="mt-16">
