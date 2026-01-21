da4bb4a7cb29e1208b7fd5e03b0ccecf4b40a29c
Sachin Katageri <sachin.kumar.katageri@gmail.com>
2026-01-21 07:19:51 +0000

    remove this sticky nav in web (_for element <PropertyStickyNav>_)

--- src/app/property/[id]/_components/property-page-content.tsx
+++ src/app/property/[id]/_components/property-page-content.tsx
@@ -109,7 +109,7 @@ export default function PropertyPageContent({ property }: PropertyPageContentPro
                                 <PropertyInfoSection property={property} />
                                 <PropertyImageGallery />
                                 
-                                <PropertyStickyNav />
+                                {isMobile && <PropertyStickyNav />}
 
                                 <PropertySpecsCard property={property} />
                                 <CustomInfrastructureCard />
